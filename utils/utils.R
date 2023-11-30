eb831b674c0d1aaf52bc645a53af9bacac2c5a15
Noëlle R <noelle.tiana@gmail.com>
2023-11-29 21:20:47 -0800

    .

---
+++ utils/utils.R
@@ -0,0 +1,13 @@
+# library(tidyverse)
+# library(dplyr)
+# library(RColorBrewer)
+# library(readr)
+# library(tidytext)
+
+install.packages("pacman")
+
+pacman::p_load(tidyverse,
+               dplyr,
+               RColorBrewer,
+               reader,
+               tidytext)
